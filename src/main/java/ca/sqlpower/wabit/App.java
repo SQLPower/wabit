b32f6a474382dd1261a2aa4b3329e8bfaa428eb5
mo.jeff <mo.jeff@78c4bb32-93da-11dd-ada7-8df265a40b68>
2008-10-06 19:17:01 +0000

    Initial Commit

---
+++ src/main/java/ca/sqlpower/wabit/App.java
@@ -0,0 +1,13 @@
+package ca.sqlpower.wabit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
