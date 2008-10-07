2774ad0da3b2e2da3ab7ccc5a331b4527b0c429c
mo.jeff <mo.jeff@78c4bb32-93da-11dd-ada7-8df265a40b68>
2008-10-07 18:49:10 +0000

    1. We don't have any use for the Hello-world App.java class
    2. WabitSwingSession should go into a swingui package.

--- src/main/java/ca/sqlpower/wabit/swingui/WabitSwingSession.java
+++ src/main/java/ca/sqlpower/wabit/swingui/WabitSwingSession.java
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
  */
 
-package ca.sqlpower.wabit;
+package ca.sqlpower.wabit.swingui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
