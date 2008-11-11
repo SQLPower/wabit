ab109e0e0a7dc9a8f98829e4442c290056fbc5ea
mo.jeff <mo.jeff@78c4bb32-93da-11dd-ada7-8df265a40b68>
2008-11-11 23:31:27 +0000

    Added an skeleton implementation of a 'JDBC data source' (implements WabitDataSource) that represents data sources accessed via the JDBC API. I haven't added anything to the WabitDataSource interface yet, as I have not quite thought up what methods will be needed for the interface that are also generic enough to apply to different types of data sources.

---
+++ src/main/java/ca/sqlpower/wabit/JDBCDataSource.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2008, SQL Power Group Inc.
+ *
+ * This file is part of Wabit.
+ *
+ * Wabit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wabit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.wabit;
+
+import java.util.List;
+
+import ca.sqlpower.sql.SPDataSource;
+
+/**
+ * An implementation of {@link WabitDataSource} that deals with data sources
+ * that are accessed using the JDBC API.
+ */
+public class JDBCDataSource extends AbstractWabitObject implements WabitDataSource {
+
+	/**
+	 * Underlying {@link SPDataSource} object that actually contains all the
+	 * database connection info.
+	 */
+	private SPDataSource dataSource;
+
+	public boolean allowsChildren() {
+		return true;
+	}
+
+	public int childPositionOffset(Class<? extends WabitObject> childType) {
+		return 0;
+	}
+
+	public List<WabitObject> getChildren() {
+		return null;
+	}
+}
