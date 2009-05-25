9bd51c2e2ad656b0c2ffd6bdc7f3a79df172ae1e
mo.jeff <mo.jeff@78c4bb32-93da-11dd-ada7-8df265a40b68>
2009-05-25 16:21:26 +0000

    Just thought it'd be nice to have the test actually stop when you close the window (so that you don't have a bunch of closed but still running test instances)

--- src/main/java/ca/sqlpower/wabit/swingui/olap/MondrianTest.java
+++ src/main/java/ca/sqlpower/wabit/swingui/olap/MondrianTest.java
@@ -22,6 +22,9 @@ package ca.sqlpower.wabit.swingui.olap;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
@@ -89,6 +92,12 @@ public class MondrianTest {
         frame.setContentPane(splitPane);
         frame.pack();
         frame.setVisible(true);
+        frame.addWindowListener(new WindowAdapter() {
+        	@Override
+        	public void windowClosing(WindowEvent e) {
+        		System.exit(0);
+        	}
+        });
     }
     
     private JComponent createGuiQueryPanel() throws SQLException {
