e2b2fcee1d515ba81544bd436b9a7dc6e919822a
thomasobrien95 <thomasobrien95@78c4bb32-93da-11dd-ada7-8df265a40b68>
2009-07-14 16:02:59 +0000

    NEW - bug 1964: NullPointerException sometimes happens when adding a new datasource while in the OLAP query editor http://trillian.sqlpower.ca/bugzilla/show_bug.cgi?id=1964
    The NewWorkspaceScreen was not removing a listener on the session when
    the frame was being disposed. This led to the listener trying to
    recreate the session's UI when a data source was added after the
    window appeared. Since the UI was being recreated the current
    CellSetViewer was losing its graphics object and causing a NPE.

--- src/main/java/ca/sqlpower/wabit/swingui/NewWorkspaceScreen.java
+++ src/main/java/ca/sqlpower/wabit/swingui/NewWorkspaceScreen.java
@@ -38,42 +38,67 @@ import ca.sqlpower.sqlobject.SQLObjectRuntimeException;
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.FormLayout;
 
+/**
+ * This class is used to display a screen for adding the first data source to a
+ * new workspace. If this screen is closed without choosing a data source the
+ * welcome screen will be displayed.
+ */
 public class NewWorkspaceScreen {
 	
 	private static Logger logger = Logger.getLogger(NewWorkspaceScreen.class);
 	
+	/**
+	 * The new session that needs a starting data source added to it.
+	 */
 	private final WabitSwingSession session;
+	
+	/**
+	 * The frame displaying data source choices to the user.
+	 */
 	private final JFrame frame;
+	
+	/**
+	 * A flag tracking if a data source was added to the workspace. Used
+	 * to decide if the session should be disposed when this window closes.
+	 */
 	private boolean databaseAdded = false;
+	
+	/**
+	 * This listener will dispose of the frame and show the session's UI when
+	 * a data source was added to the workspace.
+	 */
+	private final DatabaseListChangeListener workspaceDataSourceListener = new DatabaseListChangeListener() {
+	    public void databaseRemoved(DatabaseListChangeEvent e) {
+	        //Cannot remove databases from this panel.
+	    }
+	    
+	    public void databaseAdded(DatabaseListChangeEvent e) {
+	        logger.debug("Added data source " + e.getDataSource().getName());
+	        try {
+	            session.buildUI();
+	        } catch (SQLObjectException e1) {
+	            throw new SQLObjectRuntimeException(e1);
+	        }
+	        databaseAdded = true;
+	        frame.dispose();
+	    }
+	};
 	
 	public NewWorkspaceScreen(final WabitSwingSessionContext context) {
 		session = new WabitSwingSessionImpl(context);
 		
-		session.getWorkspace().addDatabaseListChangeListener(new DatabaseListChangeListener() {
-			public void databaseRemoved(DatabaseListChangeEvent e) {
-				//Cannot remove databases from this panel.
-			}
-		
-			public void databaseAdded(DatabaseListChangeEvent e) {
-				databaseAdded = true;
-				frame.dispose();
-				try {
-					session.buildUI();
-				} catch (SQLObjectException e1) {
-					throw new SQLObjectRuntimeException(e1);
-				}
-			}
-		});
+        session.getWorkspace().addDatabaseListChangeListener(workspaceDataSourceListener);
 		
 		frame = new JFrame();
-		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
-			public void windowClosing(WindowEvent e) {
+			public void windowClosed(WindowEvent e) {
 				if (!databaseAdded) {
 					context.deregisterChildSession(session);
 					frame.dispose();
 				}
+				session.getWorkspace().removeDatabaseListChangeListener(workspaceDataSourceListener);
 			}
 		});
 		
@@ -103,7 +128,8 @@ public class NewWorkspaceScreen {
 	public void showFrame() {
 		frame.pack();
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		frame.setLocation((int) (toolkit.getScreenSize().getWidth() / 2 - frame.getWidth() / 2), (int) (toolkit.getScreenSize().getHeight() / 2 - frame.getHeight() / 2));
+		frame.setLocation((int) (toolkit.getScreenSize().getWidth() / 2 - frame.getWidth() / 2), 
+		        (int) (toolkit.getScreenSize().getHeight() / 2 - frame.getHeight() / 2));
 		frame.setVisible(true);
 	}
 
