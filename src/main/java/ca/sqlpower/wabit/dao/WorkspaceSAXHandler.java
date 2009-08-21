a71ab515142a6ec724c11b620ed8b1360273d1c2
thomasobrien95 <thomasobrien95@78c4bb32-93da-11dd-ada7-8df265a40b68>
2009-08-21 15:44:55 +0000

    Minor bug fix for loading the automatically executing flag.

--- src/main/java/ca/sqlpower/wabit/dao/WorkspaceSAXHandler.java
+++ src/main/java/ca/sqlpower/wabit/dao/WorkspaceSAXHandler.java
@@ -399,7 +399,7 @@ public class WorkspaceSAXHandler extends DefaultHandler {
         		} else if (aname.equals("execute-queries-with-cross-joins")) {
         		    cache.setExecuteQueriesWithCrossJoins(Boolean.parseBoolean(aval));
         		} else if (aname.equals("automatically-executing")) {
-        		    cache.setAutomaticallyExecuting(Boolean.getBoolean(aval));
+        		    cache.setAutomaticallyExecuting(Boolean.parseBoolean(aval));
         		} else {
         			logger.warn("Unexpected attribute of <query>: " + aname + "=" + aval);
         		}
