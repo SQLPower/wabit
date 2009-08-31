561164a6882f887f6404abd00b83175cb5f9f49c
thomasobrien95 <thomasobrien95@78c4bb32-93da-11dd-ada7-8df265a40b68>
2009-08-31 22:32:53 +0000

    ASSIGNED - bug 2009: Add templates to report editor http://trillian.sqlpower.ca/bugzilla/show_bug.cgi?id=2009
    Added a new Blank template that will always be visible from the
    template chooser menu. Now you can create an empty report even if
    no templates have been created.

--- src/main/java/ca/sqlpower/wabit/swingui/report/TemplateChooserDialog.java
+++ src/main/java/ca/sqlpower/wabit/swingui/report/TemplateChooserDialog.java
@@ -21,6 +21,8 @@ package ca.sqlpower.wabit.swingui.report;
 
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
@@ -31,20 +33,56 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import net.miginfocom.swing.MigLayout;
+import ca.sqlpower.wabit.report.ContentBox;
+import ca.sqlpower.wabit.report.Page;
 import ca.sqlpower.wabit.report.Template;
 import ca.sqlpower.wabit.swingui.WabitSwingSession;
 import ca.sqlpower.wabit.swingui.action.ReportFromTemplateAction;
 
+/**
+ * This class creates a dialog for choosing a template for a new report.
+ * Clicking on a template will use the {@link ReportFromTemplateAction}'s
+ * action to add a new report to the workspace.
+ * <p>
+ * TODO: This class does not need to extend JDialog. The panel it creates can
+ * be placed in a new JDialog in the classes that call it.
+ */
 public class TemplateChooserDialog extends JDialog {
+
+    /**
+     * This template is a default one that will be added to the dialog if there
+     * is no templates available. This gives the user at least one option to
+     * choose from and will let them see where templates are used.
+     */
+    private static final Template DEFAULT_TEMPLATE = new Template("Blank");
+    
+    static {
+        //Add a content box to the default template and have it fill up to the margins.
+        final ContentBox centreContentBox = new ContentBox();
+        final Page page = DEFAULT_TEMPLATE.getPage();
+        page.addContentBox(centreContentBox);
+        centreContentBox.setX(page.getLeftMarginOffset());
+        centreContentBox.setY(page.getUpperMarginOffset());
+        centreContentBox.setWidth(page.getRightMarginOffset() - page.getLeftMarginOffset());
+        centreContentBox.setHeight(page.getLowerMarginOffset() - page.getUpperMarginOffset());
+    }
+    
 	private final JPanel panel = new JPanel(new MigLayout("", "[][][]"));
 	
 	public TemplateChooserDialog(final WabitSwingSession session) {
 		setTitle("Template Chooser");
 
 		int i = 0;
-		for (final Template t : session.getWorkspace().getTemplates()) {
+		List<Template> templates = new ArrayList<Template>();
+		templates.add(DEFAULT_TEMPLATE);
+		templates.addAll(session.getWorkspace().getTemplates());
+		for (final Template t : templates) {
 			JPanel previewPanel = new JPanel(new MigLayout("", "[center]"));
 			i++;
+			//TODO refactor the necessary steps from the actionPerformed method
+			//and place them in a static method in ReportFromTemplateAction instead.
+			//OR if the button does not need its visiblity changed replace the abstract
+			//action with the ReportFromTemplateAction.
 			JButton previewButton = new JButton(new AbstractAction() {
 				public void actionPerformed(ActionEvent e) {
 					setVisible(false);
