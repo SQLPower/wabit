2bf1def75b53b1f5edb729100d29439a1f581daa
jfuerth <jfuerth@78c4bb32-93da-11dd-ada7-8df265a40b68>
2009-09-02 19:39:27 +0000

    NEW - bug 2071: Make charts glossy http://trillian.sqlpower.ca/bugzilla/show_bug.cgi?id=2071
    To avoid OS X 10.4 crashes, not using GradientPaint for the chart background anymore.

--- src/main/java/ca/sqlpower/wabit/report/chart/ChartGradientPainter.java
+++ src/main/java/ca/sqlpower/wabit/report/chart/ChartGradientPainter.java
@@ -19,34 +19,77 @@
 
 package ca.sqlpower.wabit.report.chart;
 
-import java.awt.Color;
-import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Transparency;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
+import org.apache.log4j.Logger;
+
+/**
+ * Helper class for making charts look nicer.
+ */
 public class ChartGradientPainter {
 
-    private static Color topGradientColour = new Color(0xEEEEEE);
-    private static Color bottomGradientColour = new Color(0xFFFFFF);
+    private static final Logger logger = Logger.getLogger(ChartGradientPainter.class);
     
+    private static int TOP_BRIGHTNESS = 0xEE;
+    private static int BOTTOM_BRIGHTNESS = 0xFF;
+
+    /**
+     * Paints two gradients which, together, entirely fill the given rectangle.
+     * 
+     * @param g
+     *            The graphics to paint into
+     * @param bounds
+     *            The rectangle to fill with gradients
+     * @param baseline
+     *            The cutoff point from the top gradient to the bottom gradient.
+     *            This is normally set to line up with the X-axis of the chart.
+     */
     public static void paintChartGradient(Graphics2D g, Rectangle2D bounds, int baseline) {
         int x = (int) bounds.getX();
         int y = (int) bounds.getY();
         int width = (int) bounds.getWidth();
         int height = (int) bounds.getHeight();
-        
-        g.setPaint(new GradientPaint(
-                0, y, topGradientColour,
-                0, baseline, bottomGradientColour));
-        g.fillRect(x, y, width, baseline);
 
-        g.setPaint(new GradientPaint(
-                0, baseline, topGradientColour,
-                0, height, bottomGradientColour));
-        g.fillRect(
-                x, baseline,
-                width, height);
+        Object origInterpolation = g.getRenderingHint(RenderingHints.KEY_INTERPOLATION);
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+
+        // We can't use a normal GradientPaint because some interaction between
+        // GradientPaint, Piccolo, and Apple's CoreGraphics on OS X 10.4 causes
+        // a JVM crash. This buffered image scaling should be pretty performant
+        // too, and has the added bonus of not crashing.
+        BufferedImage img = makeGradientImage(g);
+        g.drawImage(img, x, y, width, baseline, 0, 0, img.getWidth(), img.getHeight(), null);
+        g.drawImage(img, x, baseline, width, height, 0, 0, img.getWidth(), img.getHeight(), null);
         
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, origInterpolation);
     }
 
+    /**
+     * Creates a buffered image compatible with the given graphics with a width
+     * of 1 and a height exactly enough to have one pixel of each colour between
+     * {@link #TOP_BRIGHTNESS} and {@link #BOTTOM_BRIGHTNESS}.
+     * 
+     * @param g The graphics the generated image should be compatible with.
+     * @return A BufferedImage as described above.
+     */
+    private static BufferedImage makeGradientImage(Graphics2D g) {
+        if (BOTTOM_BRIGHTNESS < TOP_BRIGHTNESS) {
+            throw new IllegalStateException("This code only handles dark-to-light gradients");
+        }
+        BufferedImage img = g.getDeviceConfiguration().createCompatibleImage(
+                1, BOTTOM_BRIGHTNESS - TOP_BRIGHTNESS + 1, Transparency.OPAQUE);
+        int[] gradient = new int[img.getHeight()];
+        for (int i = 0; i < img.getHeight(); i++) {
+            int b = (TOP_BRIGHTNESS + i) & 0xff;
+            int rgb = 0xff000000 | (b << 16) | (b << 8) | b;
+            gradient[i] = rgb;
+            logger.debug(String.format("Set pixel %d to %x", i, gradient[i]));
+        }
+        img.setRGB(0, 0, 1, img.getHeight(), gradient, 0, 1);
+        return img;
+    }
 }
