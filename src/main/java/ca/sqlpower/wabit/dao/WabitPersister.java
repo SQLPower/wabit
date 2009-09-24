743fa8136143b00b96dfae0cf5d83f005297fb44
mo.jeff <mo.jeff@78c4bb32-93da-11dd-ada7-8df265a40b68>
2009-09-24 18:32:52 +0000

    Made some modifications to the WabitPersister interface based on feedback. One open question is if the interface should allow for nested transactions (ie. if you call begin() after another begin() without calling commit(), what happens?)

--- src/main/java/ca/sqlpower/wabit/dao/WabitPersister.java
+++ src/main/java/ca/sqlpower/wabit/dao/WabitPersister.java
@@ -29,57 +29,116 @@ import ca.sqlpower.wabit.WabitWorkspace;
 public interface WabitPersister {
 
 	/**
-	 * Persists a property into a WabitObject. It may throw an {@link Exception}
-	 * if the actual previous value in persistent storage does not match the
-	 * expected previous value as an indication to the object using this
-	 * WabitPersister that their cached copy of the {@link WabitObject} may be
-	 * out of sync with the persistent storage.
+	 * Modifies the named property of the specified WabitObject in this
+	 * persister's workspace. It may throw an {@link Exception} if the actual
+	 * previous value in persistent storage does not match the expected previous
+	 * value as an indication to the object using this WabitPersister that their
+	 * cached copy of the {@link WabitObject} may be out of sync with the
+	 * persistent storage.
 	 * 
-	 * @param object
-	 *            The {@link WabitObject} in which to set the property
+	 * @param uuid
+	 *            The UUID of the {@link WabitObject} in which to set the
+	 *            property
 	 * @param propertyName
-	 *            The name of the property to persist
+	 *            The JavaBeans property name of the property that changed, as
+	 *            it would be discovered by the java.beans.Introspector class
 	 * @param oldValue
 	 *            The expected previous value of the property
 	 * @param newValue
-	 *            The value to set the property to
+	 *            The new value to set for the property
+	 * @throws WabitPersistenceException
+	 *             A general Exception that is thrown if any Exception occurs
+	 *             while persisting the property. It can be used to wrap the
+	 *             specific cause Exception and provide other details like the
+	 *             WabitObject UUID. Some potential exceptional situations
+	 *             include:
+	 *             <ul>
+	 *             <li>The WabitObject UUID is unknown to this persister</li>
+	 *             <li>The given property name does not exist</li>
+	 *             <li>The given property is not writable</li>
+	 *             <li>The property type of the given old/new values do not
+	 *             match each other or the actual property in the object</li>
+	 *             <li>The existing persistent value doesn't match expected
+	 *             oldValue</li>
+	 *             </ul>
 	 */
-	public void persistProperty(WabitObject object, String propertyName, Object oldValue, Object newValue);
+	public void persistProperty(String uuid, String propertyName, Object oldValue, Object newValue) throws WabitPersistenceException;
 
 	/**
 	 * Adds a {@link WabitObject} into the persistent storage. If the
-	 * WabitObject has children, then they will also be persisted. If the
 	 * WabitObject already exists in persistent storage, then it will update the
-	 * existing WabitObject instead of creating a new one.
+	 * existing WabitObject instead of creating a new one. Note that this will
+	 * not persist its properties or any child objects.
 	 * 
-	 * @param parent
-	 *            The parent {@link WabitObject} of the object to persist. If
-	 *            the WabitObject has no parent (as is the case for
+	 * @param parentUUID
+	 *            The UUID of the parent {@link WabitObject} of the object to
+	 *            persist. If the WabitObject has no parent (as is the case for
 	 *            {@link WabitWorkspace}), then it can be set to null.
-	 * @param child
-	 *            The object to actually persist
+	 * @param type
+	 *            A String of the class name of the WabitObject to be persisted
+	 *            (ex. WabitWorkspace)
+	 * @param uuid
+	 *            The UUID of the {@link WabitObject} to actually persist
+	 * @throws WabitPersistenceException
+	 *             A general Exception that is thrown if any Exception occurs
+	 *             while persisting the WabitObject. It can be used to wrap the
+	 *             specific cause Exception and provide other details like the
+	 *             WabitObject UUID. Some potential exceptional situations
+	 *             include:
+	 *             <ul>
+	 *             <li>A WabitObject with the given UUID already exists in the
+	 *             persistent storage</li>
+	 *             <li>A WabitObject with the given parent UUID does not exist</li>
+	 *             </ul>
 	 */
-	public void persistObject(WabitObject parent, WabitObject child);
-	
+	public void persistObject(String parentUUID, String type, String uuid) throws WabitPersistenceException;
+
 	/**
 	 * Removes a WabitObject from persistent storage
 	 * 
-	 * @param parent
-	 *            The parent {@link WabitObject} of the object to remove
-	 * @param child
-	 *            The {@link WabitObject} to remove
+	 * @param parentUUID
+	 *            The UUID of the parent {@link WabitObject} of the object to
+	 *            remove
+	 * @param uuid
+	 *            The UUID of the {@link WabitObject} to remove
+	 * @throws WabitPersistenceException
+	 *             A general Exception that is thrown if any Exception occurs
+	 *             while persisting the WabitObject. It can be used to wrap the
+	 *             specific cause Exception and provide other details like the
+	 *             WabitObject UUID.
 	 */
-	public void removeObject(WabitObject parent, WabitObject child);
+	public void removeObject(String parentUUID, String uuid) throws WabitPersistenceException;
 	
 	/**
 	 * Indicates the start of an atomic transaction of persisting multiple
 	 * {@link WabitObject}s
 	 */
 	public void begin();
-	
+
 	/**
 	 * Causes a current {@link WabitObject} persistence transaction to commit
 	 * its results.
+	 * 
+	 * @throws WabitPersistenceException
+	 *             A general Exception that is thrown if any Exception occurs
+	 *             while committing the transaction. This could be caused by one
+	 *             being thrown by {@link #persistObject(String, String)},
+	 *             {@link #persistProperty(String, String, Object, Object)}, or
+	 *             {@link #removeObject(String, String)}. Some other exceptional
+	 *             situations include:
+	 *             <ul>
+	 *             <li>An update conflict with another transaction</li>
+	 *             <li>Network issues</li>
+	 *             <li>Insufficient permissions in the backing store</li>
+	 *             </ul>
 	 */
-	public void commit();
+	public void commit() throws WabitPersistenceException;
+
+	/**
+	 * Restores the persisted WabitObject back to the state it was in before the
+	 * transaction began (i.e. when the call to {@link #begin()} was made).
+	 * Typically, this would be called if an exception occurs during an atomic
+	 * transaction,
+	 */
+	public void rollback();
 }
