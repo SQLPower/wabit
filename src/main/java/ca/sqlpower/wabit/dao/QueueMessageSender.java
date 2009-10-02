03be9f562bfaaa88d36b0fb060147337e1d35dc9
mo.jeff <mo.jeff@78c4bb32-93da-11dd-ada7-8df265a40b68>
2009-10-02 14:29:02 +0000

    Added an implementation of MessageSender that places it's messages into a Queue. We can use this with a SynchronousQueue to send update notifications from the server to the clients.

---
+++ src/main/java/ca/sqlpower/wabit/dao/QueueMessageSender.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2009, SQL Power Group Inc.
+ *
+ * This file is part of Wabit.
+ *
+ * Wabit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wabit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.wabit.dao;
+
+import java.util.Queue;
+
+/**
+ * A {@link MessageSender} implementation that places message contents onto a
+ * Queue.
+ * 
+ * @param <T>
+ *            The Object type that will represent message content
+ */
+public class QueueMessageSender<T> implements MessageSender<T> {
+
+	private final Queue<T> queue;
+
+	public QueueMessageSender(Queue<T> queue) {
+		this.queue = queue;
+	}
+
+	public Queue<T> getQueue() {
+		return queue;
+	}
+
+	public void send(T content) throws WabitPersistenceException {
+		queue.add(content);
+	}
+}
