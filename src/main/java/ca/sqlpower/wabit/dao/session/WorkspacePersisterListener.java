ab99677db5d12ea1e99159e66b036ffe0a36f18e
mc3ander <mc3ander@uwaterloo.ca>
2009-10-16 18:08:55 +0000

    Changed WorkspacePersisterListener to not perist workspaces.

--- src/main/java/ca/sqlpower/wabit/dao/session/WorkspacePersisterListener.java
+++ src/main/java/ca/sqlpower/wabit/dao/session/WorkspacePersisterListener.java
@@ -227,8 +227,8 @@ public class WorkspacePersisterListener implements WabitListener {
 		int index = 0;
 		if (root.getParent() != null) {
 			index = root.getParent().getChildren().indexOf(root);
+			persistChild(root.getParent(), root, root.getClass(), index);
 		}
-		persistChild(root.getParent(), root, root.getClass(), index);
 		for (WabitObject child : root.getChildren()) {
 			persistObject(child);
 		}
