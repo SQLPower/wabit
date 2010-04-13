6765ea6cce3538f5d4c708d7f28816ec5962a327
lucboudreau <lucboudreau@78c4bb32-93da-11dd-ada7-8df265a40b68>
2010-04-13 16:02:37 +0000

    Removes unused import.

--- src/main/java/ca/sqlpower/wabit/rs/ResultSetHandle.java
+++ src/main/java/ca/sqlpower/wabit/rs/ResultSetHandle.java
@@ -30,7 +30,6 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.annotation.Nonnull;
